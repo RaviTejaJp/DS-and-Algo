7c5d4a603d97e858b55740dec44289ca6a25063d
Ravi Teja <rteja@omz.com>
2023-11-28 12:12:48 +0530

    leet code simplyfy path

--- src/datastructures/stack/stack_list.py
+++ src/datastructures/stack/stack_list.py
@@ -1,34 +1,37 @@
-from typing import Any, List
+from __future__ import annotations
+
+from typing import Any
+from typing import List
 
 
 class StackList:
-    def __init__(self, length: int=10) -> None:
+    def __init__(self, length: int = 10) -> None:
         self.stack = []
         self.top = -1
         self.length = length
 
     def push(self, data: Any) -> None:
-        if self.top <= self.length-1:
+        if self.top <= self.length - 1:
             self.stack.append(data)
             self.top += 1
-            print(f"{data} is successfully pushed to stack")
+            print(f'{data} is successfully pushed to stack')
         else:
-            raise MemoryError("Stack is full")
+            raise MemoryError('Stack is full')
 
     def pop(self) -> Any:
         popped_element: Any = None
         if self.top <= -1:
-            raise IndexError("Stack is empty")
+            raise IndexError('Stack is empty')
         else:
             self.top -= 1
             popped_element = self.stack.pop()
-            print(f"{popped_element} is successfully popped from stack")
+            print(f'{popped_element} is successfully popped from stack')
         return popped_element
 
     def peek(self) -> Any:
         peeked_element: Any = None
         if self.top <= -1:
-            raise IndexError("Stack is empty")
+            raise IndexError('Stack is empty')
         else:
             peeked_element = self.stack[-1]
         return peeked_element
@@ -50,17 +53,17 @@ class StackList:
         return str(list(self.stack))
 
     def __str__(self) -> str:
-        stack_str: str = "Top : "
+        stack_str: str = 'Top : '
         for x in range(len(self.stack) - 1, -1, -1):
-            stack_str = stack_str + f" {self.stack[x]} ->"
-        stack_str += " Done"
-        return f"Stack : {stack_str}"
+            stack_str = stack_str + f' {self.stack[x]} ->'
+        stack_str += ' Done'
+        return f'Stack : {stack_str}'
 
     def __iter__(self) -> str:
         for item in range(len(self.stack) - 1, -1, -1):
             yield self.stack[item]
-    
+
     def isFull(self) -> bool:
-        if self.length == self.top+1:
+        if self.length == self.top + 1:
             return True
         return False
