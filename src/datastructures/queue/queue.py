53917a8d8eee3fb343eac4df37de542bee657c28
Ravi Teja <rteja@omz.com>
2024-01-03 13:37:31 +0530

    Queue using Linked list

--- src/datastructures/queue/queue.py
+++ src/datastructures/queue/queue.py
@@ -1,4 +1,5 @@
-from typing import Any
+from __future__ import annotations
+from typing import Any, Type
 
 
 class Queue:
@@ -106,3 +107,92 @@ class CircularQueue:
             while temp <= self.end:
                 yield self.circular_queue[temp]
                 temp = temp + 1
+
+class LinkedListNode:
+    def __init__(self,data:Any, next: LinkedListNode | None =None) -> None:
+        self._data = data
+        self._next = next
+    
+    @property
+    def data(self)-> Any:
+        return self._data
+    
+    @data.setter
+    def data(self, data: Any) -> None:
+        self._data = data
+    
+    @property
+    def next(self) -> LinkedListNode | None:
+        return self._next
+    
+    @next.setter
+    def next(self, value: LinkedListNode | None) -> None:
+        if isinstance(value, LinkedListNode) or value is None:
+            self._next = value
+        else:
+            return f"Value must be of type {self.__class__.__name__} not of {type(value)}"
+
+class LinkedList:
+    def __init__(self)-> None:
+        self.head = None
+        self.tail = None
+    
+    def __iter__(self):
+        temp = self.head
+        while temp:
+            yield temp.data
+            temp = temp.next
+
+    def append(self,data:Any)-> None:
+        temp = LinkedListNode(data=data)
+        if self.head:
+            self.tail.next = temp
+            self.tail = temp
+        else:
+            self.head = temp
+            self.tail = temp
+    
+    def pop_first(self)-> LinkedListNode:
+        try:
+            if self.head:
+                temp = self.head
+                self.head = self.head.next
+                if self.head is None:
+                    self.tail = None
+            else:
+                raise ValueError("Can not pop from empty Linked List")
+        except Exception as e:
+            return None
+        finally:
+            return temp.data
+
+class LLQueue:
+    def __init__(self):
+        self.llq = LinkedList()
+    
+    def enqueue(self,data):
+        self.llq.append(data)
+    
+    def dequeue(self) -> Any:
+        return self.llq.pop_first()
+    
+    def peek(self) -> Any:
+        if self.llq.head:
+            return self.llq.head.data
+        else:
+            return "Empty Queue"
+    
+    def isEmpty(self) -> bool:
+        if self.llq.head:
+            return False
+        return True
+
+    def delete(self)-> None:
+        self.llq.head = None
+        self.llq.tail = None
+
+    def to_list(self) -> list:
+        return list(self.llq)
+    
+    def __iter__(self):
+        return iter(self.llq)
